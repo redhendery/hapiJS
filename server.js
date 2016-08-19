e4f3674a3b3261862e7b723f01bd701bb3d8a240
redhendery <redhendery@gmail.com>
2016-08-20 10:05:27 +1200

    update user details

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ server.register(require('inert'), (err) => {
 // Hapi-Basic-Auth
 const users = {
   john: {
-    username: 'john',
-    password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
+    username: 'john@john.com',
+    password: 'password',   // 'secret'
     name: 'John Doe',
     id: '2133d32a'
   }
