452a4ca22fab94c6d910d431f8af55d95bfbc18d
redhendery <redhendery@gmail.com>
2016-08-20 10:01:11 +1200

    add debug log to gitignore, add basic auth

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Bcrypt = require('bcrypt');
+const Basic = require('hapi-auth-basic');
 
 const server = new Hapi.Server();
 server.connection({ port: 3000 });
-
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    reply('Hello, world!');
-  }
-});
 
 server.route({
   method: 'GET',
@@ -40,4 +34,44 @@ server.register(require('inert'), (err) => {
       reply.file('./public/hello.html');
     }
   });
+});
+
+// Hapi-Basic-Auth
+const users = {
+  john: {
+    username: 'john',
+    password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
+    name: 'John Doe',
+    id: '2133d32a'
+  }
+};
+
+const validate = function (request, username, password, callback) {
+  const user = users[username];
+  if (!user) {
+    return callback(null, false);
+  }
+
+  Bcrypt.compare(password, user.password, (err, isValid) => {
+    callback(err, isValid, { id: user.id, name: user.name });
+  });
+};
+
+server.register(Basic, (err) => {
+  if (err) {
+    throw err;
+  }
+
+  server.auth.strategy('simple', 'basic', { validateFunc: validate });
+  server.route({
+    method: 'GET',
+    path: '/',
+    config: {
+      auth: 'simple',
+      handler: function (request, reply) {
+        reply('hello, ' + request.auth.credentials.name);
+      }
+    }
+  });
+
 });
